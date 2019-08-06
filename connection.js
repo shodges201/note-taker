b8969638ad90e8525d0e1287296040fd103156d5
Scott Hodges <shodges201@gmail.com>
2019-08-06 18:49:58 -0400

    connected mysql connection to jaws db database

--- connection.js
+++ connection.js
@@ -6,11 +6,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "jsk3f4rbvp8ayd7w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "", // add your local password here.
-    database: "" // add your db name here
+    user: "z457lkzecn130zh0",
+    password: "uhjyb4033vuf9kvp", // add your local password here.
+    database: "xfptegwfug2n50u0" // add your db name here
   });
 }
 
